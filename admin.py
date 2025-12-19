c5e70c53726c090da2d4061ce1701dcc130190e5
malavikamb2k1-create <malavikamb2k1@gmail.com>
2025-12-19 08:41:11 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import SportsCategory, City, Area, SportsEvent
+
+admin.site.register(SportsCategory)
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(SportsEvent)
