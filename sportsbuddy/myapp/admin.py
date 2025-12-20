107dd7cbb0f66ba0de748343c41792924de2096d
malavikamb2k1-create <malavikamb2k1@gmail.com>
2025-12-20 09:51:30 +0530

    Update admin.py

--- sportsbuddy/myapp/admin.py
+++ sportsbuddy/myapp/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import SportsCategory, City, Area, SportsEvent
+
+admin.site.register(SportsCategory)
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(SportsEvent)
 
