31aae26c9491ba944e61c1b79d96847d209f1acd
malavikamb2k1-create <malavikamb2k1@gmail.com>
2025-12-20 09:29:55 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SportsCategory, City, Area, SportsEvent
-
-admin.site.register(SportsCategory)
-admin.site.register(City)
-admin.site.register(Area)
-admin.site.register(SportsEvent)
